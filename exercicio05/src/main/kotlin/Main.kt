a0a64f34b76174d3295ebcdf2e9972249fb8cb0d
nickolss <nickolasmaiaraujo@gmail.com>
2024-03-10 16:37:37 -0300

    Adicionado exercicio05

---
+++ exercicio05/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("There is a chance of snow")
+}
